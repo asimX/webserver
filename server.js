b1a0d77c8fec0edff0776a86cd2254b9c0cc1bf3
Asim Siddiqui <asim@blackwidowapps.com>
2016-02-02 16:14:02 -0800

    Initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+/**
+ * Created by asiddiqui on 2/2/16.
+ */
+
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+    requireAuthentication: function(req, res, next){
+        console.log('private route hit!');
+        next();
+    },
+    logger: function(req, res, next){
+        console.log('Request: ' + new Date().toString()+ ' ' + req.method + ' ' +req.originalUrl);
+        next();
+    }
+}
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req,res){
+    res.send('about us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+//app.get('/public')
+
+app.listen(PORT, function(){
+    console.log('Express server started at '+ PORT);
+});
